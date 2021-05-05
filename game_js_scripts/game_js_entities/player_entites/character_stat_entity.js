5320dee62bdecedc7e08ed6e53772bf6ee0ff9f4
Benoy sen <benoysen@silverhawk.co.uk>
2021-05-05 20:37:33 +0100

    update player stats

--- game_js_scripts/game_js_entities/player_entites/character_stat_entity.js
+++ game_js_scripts/game_js_entities/player_entites/character_stat_entity.js
@@ -8,11 +8,52 @@ game.character_stat_entity = me.Entity.extend(
   /**********************************************/ 
   character_stats_data : 
   {
-    player_initial_level = 1,
-    player_initial_gold = 0,
-    player_initial_experience = 0,
-    player_initial_summoning = 30,
-    player_initial_summoning_interval = 3000,
+    player_stats :
+    {
+      player_initial_level : 1,
+      player_initial_experience : 0,
+      player_initial_max_health : 100,
+      player_initial_current_health : 100,
+      player_initial_max_energy : 100,
+      player_initial_current_energy : 100,
+      player_initial_max_mana : 100,
+      player_initial_current_mana : 100,
+      player_initial_gold : 0,
+      player_initial_summoning : 30,
+      player_initial_summoning_interval : 3000,
+    },
+
+    player_skills :
+    {
+      player_fighting :
+      {
+        player_level : 1,
+        player_experience : 0,
+      },
+
+      player_spellcasting : 
+      {
+        player_level : 1,
+        player_experience : 0,
+      },
+
+      player_archery : 
+      {
+        player_level : 1,
+        player_experience : 0,
+      },
+
+      player_crafting : 
+      {
+        player_level : 1,
+        player_experience : 0,
+      },
+
+      player_lockpicking : 
+      {
+        player_level : 1,
+        player_experience : 0,
+      },
+    }
   }
-   
 });
