884430465c36a312ba95f7e0885cc90bb5c39eb3
Benoy sen <benoysen@silverhawk.co.uk>
2021-05-07 09:22:17 +0100

    new trap added to game

--- game_js_scripts/game_js_entities/platform_entites/explosive_trap_entity.js
+++ game_js_scripts/game_js_entities/platform_entites/explosive_trap_entity.js
@@ -8,10 +8,18 @@ game.explosive_trap_entity = me.Entity.extend(
   /***************/
   init: function(x, y, settings) 
   {
-    this.explosive_trap_flicker_speed = settings.explosive_trap_flicker_speed;
-    this.explosive_trap_hit_points = settings.explosive_trap_hit_points;
-    this.explosive_damage_max_distance = settings.explosive_damage_max_distance;
+    /**************************************************************************/
+    /* explosive_trap_animation_speed = how fast the animation plays for trap */
+    /* explosive_trap_time_delay = delay on trap triggering                   */
+    /* explosive_trap_hit_points = how much does it hurt the player           */
+    /* explosive_trap_damage_distance = how far does the damage extend        */
+    /* explosive_trap_damage_falloff = how tha damage fall off over distance  */
+    /**************************************************************************/
+    this.explosive_trap_animation_speed = settings.explosive_trap_animation_speed;
     this.explosive_trap_time_delay = settings.explosive_trap_time_delay;
+    this.explosive_trap_hit_points = settings.explosive_trap_hit_points;
+    this.explosive_trap_damage_distance = settings.explosive_trap_damage_max_distance;
+    this.explosive_trap_damage_falloff= settings.explosive_trap_damage_falloff;
     settings.image = "explosive_trap";
     settings.shapes[0] = new me.Rect(0, 0, settings.framewidth, settings.frameheight);
 
@@ -32,7 +40,7 @@ game.explosive_trap_entity = me.Entity.extend(
     // ALWAYS update.
     this.alwaysUpdate = false;
 
-    this.renderable.addAnimation( "static_explosive_trap", [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0], this.explosive_trap_flicker_speed );
+    this.renderable.addAnimation( "static_explosive_trap", [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0], this.explosive_trap_animation_speed );
     this.renderable.setCurrentAnimation("static_explosive_trap");
   },
   
